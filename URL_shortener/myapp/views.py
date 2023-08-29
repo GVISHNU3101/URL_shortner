e93cc3c09d5f72f969d2a9b0bbc48559c778262e
GVISHNU3101 <84287406+GVISHNU3101@users.noreply.github.com>
2023-08-29 16:59:44 +0530

    Add files via upload

---
+++ URL_shortener/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def hello_world(request):
+    return HttpResponse("Hello World")
+
+def home_page(request):
+    return render(request,"index.html")
+
+def task(request):
+    return render(request,"task.html")
